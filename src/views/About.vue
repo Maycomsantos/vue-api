c938095308590de69aec71bd05790eadc3ead807
michael santos <michaelsantos.the@hotmail.com>
2021-04-18 01:55:08 -0300

    inicio

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>Esse projeto é somente para listar uma lista vindo das Api's do Pokemon</h1>
+    
+  </div>
+  
+</template>
---
+++ src/views/List.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app">
+    <h1>Testando essa lista aqui</h1>  
+    <div v-for="(poke,index) in pokemons" :key="index"> 
+      <Pokemon :name="poke.name" :url="poke.url" :num="index+1" />
+    </div>  
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+import Pokemon from '../components/Pokemon';
+
+export default ({
+  name: 'App',
+  data() {
+    return {
+      pokemons: []
+    }
+  },
+  components: {
+    Pokemon
+  },
+  created: function() {
+    axios.get('https://pokeapi.co/api/v2/pokemon').then(res => {
+      console.log('Peguei toda lista');
+      this.pokemons = res.data.results
+    })
+  }
+});
+
+</script>
